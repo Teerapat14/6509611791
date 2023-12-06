4704e39a8aefe3787594918edb0d76b65cb559de
Teerapat6509611791 <145380344+Teerapat14@users.noreply.github.com>
2023-12-06 14:31:36 +0700

    Update JUNITStack.java

--- src/core/JUNITStack.java
+++ src/core/JUNITStack.java
@@ -3,29 +3,46 @@ package core;
 import junit.framework.TestCase;
 
 public class JUNITStack extends TestCase {
-  
-	public void testCreateNewEmptyStack () {
-		Stack s1 = new Stack() ;
-		int size = s1.getSize ();
+	
+	public void testCreateNewEmptyStack() {
+		        
+		Stack s1 = new Stack();
+		int size = s1.getSize();
 		
 		assertEquals(0, size);
 		assertTrue(s1.isEmpty());
-}
-	public void testPushElmToTop() {
-		Stack s1 = new Stack ();
-		s1.push(1);
-		int  size = s1.getSize();
-		assertEquals(1, size);
-		assertTrue(s1.isEmpty());
-	}
-	public void testPushDifferentTypeElement () {
-		Stack s1 = new Stack ();
-		try {
-			s1.push((Integer) Integer.parseInt("String"));
-			fail("Expected an Exception");
-		}catch (NumberFormatException e) {
-			assertEquals("For input string : \"string\"", e.getMessage());
-			}
-		}
 	}
 	
+	public void testPushElmToTop(){
+		Stack s1 = new Stack();
+		s1.push(1);
+		int size = s1.getSize();
+		
+		assertEquals(1, size);
+		boolean empty = s1.isEmpty();
+		assertFalse(empty);
+		
+	}
+	
+	public void testPushAndPop() {
+		Stack s1 = new Stack();
+	    
+		s1.push(1);
+		s1.push(2);
+		s1.push(3);
+		
+		int popitem = s1.pop();
+		
+		
+	}
+	
+	public void testIsFull() {
+		Stack s1 = new Stack();
+		s1.push(1);
+		s1.push(2);
+		
+		assertEquals(2, s1.getSize());
+		assertTrue(s1.isFull());
+	}
+
+}
